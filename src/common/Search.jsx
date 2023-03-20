c8d6f484e54dfbb3fcc1f4b795de5a0e15f9f5e1
keya254 <brianosiemokeya@gmail.com>
2023-03-20 13:46:26 +0300

    added serch input and styles

--- src/common/Search.jsx
+++ src/common/Search.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import './nav.css'
 
 function Search() {
   return (
-    <div>Search</div>
+    <>
+    <div className="search-container">
+      <div className="search-icon">
+      <FontAwesomeIcon icon={faSearch} />
+        </div>
+        <input type="text" class="search-input" placeholder="Search products"></input>
+      </div>
+    </>
   )
 }
 
