7cd9d245624a7b218a37d104589d97d1cde399ff
keya254 <brianosiemokeya@gmail.com>
2023-03-21 11:27:02 +0300

    re arranged images dolder added styling to products

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,29 @@
-import React from 'react'
-import products from './products.json'
+import React from "react";
+import products from "./products.json";
+import "./style.css";
 
 const Home = () => {
   return (
     <>
-    <div className="products">
-  {products.map(product => {
-    return (
-      <div key={product.id} className="prodTitle">
-        <img src={product.image} alt={`Preview of ${product.title}`} />
-        <h3>{ product.title }</h3>
-        <p>{ product.description }</p>
-        <p>${ product.price }</p>
-        <p>
-          <button>Add to Cart</button>
-        </p>
+      <div className="container">
+        <div className="products">
+          {products.map((product) => {
+            return (
+              <div key={product.id} className="prodTitle">
+                <img src={process.env.PUBLIC_URL +product.image} alt={`Preview of ${product.title}`} />
+                <h3>{product.title}</h3>
+                <p>{product.description}</p>
+                <p>${product.price}</p>
+                <p>
+                  <button>Add to Cart</button>
+                </p>
+              </div>
+            );
+          })}
+        </div>
       </div>
-    );
-  })}
-</div>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
